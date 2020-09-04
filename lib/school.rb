b67be1b340e81e770e662ab619c417e5d83a81f7
Taylor Sieling <taylorasieling@gmail.com>
2020-09-04 19:55:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   
   def add_student(student_name, grade)
     @roster[grade] ||= []
-    @roster[grade] << "student_name"
+    @roster[grade] << student_name
   end
   
   def grade(grade)
